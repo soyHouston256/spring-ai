60441ad236082e074aa4a411c50fdca9d0bc0625
Max Houston Ramirez Martel <8385004+soyHouston256@users.noreply.github.com>
2026-03-02 14:34:18 -0500

    feat: add responseEntity

--- src/main/java/com/houston/streaming/web/web/controller/MovieController.java
+++ src/main/java/com/houston/streaming/web/web/controller/MovieController.java
@@ -2,6 +2,7 @@ package com.houston.streaming.web.web.controller;
 
 import com.houston.streaming.dominio.dto.MovieDto;
 import com.houston.streaming.dominio.service.MovieService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,12 +17,20 @@ public class MovieController {
     }
 
     @GetMapping("/movie")
-    public List<MovieDto> getMovies() {
-        return this.movieService.getAll();
+    public ResponseEntity<List<MovieDto>> getMovies() {
+        List<MovieDto> movieDtos = this.movieService.getAll();
+        if (movieDtos == null || movieDtos.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(movieDtos);
     }
 
     @GetMapping("/movie/{id}")
-    public MovieDto getMovie(@PathVariable long id) {
-        return this.movieService.getById(id);
+    public ResponseEntity<MovieDto> getMovie(@PathVariable long id) {
+        MovieDto movieDto = this.movieService.getById(id);
+        if(movieDto == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(movieDto);
     }
 }
